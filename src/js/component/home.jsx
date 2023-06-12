fe4d183d07403fea3b8ccd73f2d05c0d9a028e57
JSamper <60151922+jorgesamper@users.noreply.github.com>
2023-06-12 19:35:31 +0000

    Do List exercise

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [value, setValue] = useState("");
+  const [list, setList] = useState(["Recoger lo que sea"]);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleClick = () => {
+    setList((prev) => [...prev, value]);
+    setValue("");
+  };
+
+  const handleDelete = (index) => {
+    setList((prev) => prev.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-5">To-Do List!</h1>
+      <input onChange={handleChange} value={value} />
+      <button onClick={handleClick} className="btn btn-success">
+        Send
+      </button>
+
+      <br />
+      {list.map((toDo, index) => (
+        <div key={index}>
+          {index + 1}. {toDo}
+          <button onClick={() => handleDelete(index)}>X</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
