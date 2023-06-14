4ed6852d5e9ccfbdbfb87a64687efd8b4c5802cb
Jorge Samper <jsamp94@outlook.com>
2023-06-14 19:34:55 +0000

    visual design

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,8 +9,10 @@ const Home = () => {
   };
 
   const handleClick = () => {
-    setList((prev) => [...prev, value]);
-    setValue("");
+    if (value.trim() !== "") {
+      setList((prev) => [...prev, value]);
+      setValue("");
+    }
   };
 
   const handleDelete = (index) => {
@@ -18,20 +20,28 @@ const Home = () => {
   };
 
   return (
-    <div className="text-center">
-      <h1 className="text-center mt-5">To-Do List!</h1>
-      <input onChange={handleChange} value={value} />
-      <button onClick={handleClick} className="btn btn-success">
-        Send
-      </button>
+    <div className="text-center bg-light shadow-sm mx-3">
+      <h1 className="text-center mt-5 pt-2">ToDo List!</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleClick();
+        }}
+      >
+        <input onChange={handleChange} value={value} className="border w-100 bg-light " />
+      
+      </form>
 
       <br />
       {list.map((toDo, index) => (
-        <div key={index}>
-          {index + 1}. {toDo}
-          <button onClick={() => handleDelete(index)}>X</button>
+        <div className="d-flex justify-content-center border-bottom">
+          <div key={index} className="text-start mx-2">
+          {toDo}
+            <button onClick={() => handleDelete(index)} className="btn">X</button>
         </div>
+      </div>
       ))}
+      <span className="fw-bold text-primary pb-4 ">TOTAL Nº{list.length}</span>
     </div>
   );
 };
