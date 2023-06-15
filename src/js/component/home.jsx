d06b1c47d70b6406fc7e792d2734fc23da3fe8ca
Jorge Samper <jsamp94@outlook.com>
2023-06-15 11:46:07 +0000

    finish visual design

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 const Home = () => {
   const [value, setValue] = useState("");
-  const [list, setList] = useState(["Recoger lo que sea"]);
+  const [list, setList] = useState(["First task and not least"]);
+  const [hoverIndex, setHoverIndex] = useState(-1);
 
   const handleChange = (e) => {
     setValue(e.target.value);
@@ -21,27 +22,46 @@ const Home = () => {
 
   return (
     <div className="text-center bg-light shadow-sm mx-3">
-      <h1 className="text-center mt-5 pt-2">ToDo List!</h1>
+      <h1 className="text-center mt-5 pt-2">TodoList!</h1>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           handleClick();
         }}
       >
-        <input onChange={handleChange} value={value} className="border w-100 bg-light " />
-      
+        <input
+          onChange={handleChange}
+          value={value}
+          placeholder="Write a task..."
+          className="border w-100 bg-light text-center"
+        />
       </form>
 
       <br />
       {list.map((toDo, index) => (
-        <div className="d-flex justify-content-center border-bottom">
-          <div key={index} className="text-start mx-2">
-          {toDo}
-            <button onClick={() => handleDelete(index)} className="btn">X</button>
+        <div
+          key={index}
+          className="d-flex justify-content-start border-bottom"
+          onMouseEnter={() => setHoverIndex(index)}
+          onMouseLeave={() => setHoverIndex(-1)}
+        >
+          <div className="text-start mx-3 d-flex align-items-center">
+            {toDo}
+          </div>
+          {hoverIndex === index && (
+            <button
+            onClick={() => handleDelete(index)}
+            className="btn ms-auto text-end mx-2"
+            style={{ color: "red" }}
+            >
+              X
+            </button>
+          )}
         </div>
-      </div>
       ))}
-      <span className="fw-bold text-primary pb-4 ">TOTAL Nº{list.length}</span>
+      <span className="fw-bold text-primary mb-4">
+        TOTAL Nº{list.length}
+      </span>
     </div>
   );
 };
